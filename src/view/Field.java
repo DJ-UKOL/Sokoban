9ebe7a85a70c3ca25530fc52645bc387542f79b9
DJ-UKOL <dj-ukol@yandex.ru>
2022-09-29 15:26:21 +0500

    Add class Field.java and changed Controller.java, View.java.

---
+++ src/view/Field.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JPanel {
+    View view;
+
+    public Field(View view){
+        this.view = view;
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}

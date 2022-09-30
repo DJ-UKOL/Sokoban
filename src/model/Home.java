7fa8df8beb41f48cdd5fcc1c4586f9203f6a7207
DJ-UKOL <dj-ukol@yandex.ru>
2022-09-30 11:12:32 +0500

    Add class Home.java

---
+++ src/model/Home.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.awt.*;
+/*
+ * класс будет отвечать за места на игровом поле (дома) в которые нужно сдвинуть все ящики
+ */
+public class Home extends GameObject {
+
+    public Home(int x, int y) {
+        super(x, y, 2, 2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.RED);
+        graphics.drawOval(getX() - getWidth() / 2, getY() - getHeight() / 2, getWidth(), getHeight());
+    }
+}

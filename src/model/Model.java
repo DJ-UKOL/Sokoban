5a0d85e69a3c4033b111bc8f3da61a6290874cc8
DJ-UKOL <dj-ukol@yandex.ru>
2022-09-30 22:18:12 +0500

    Add levels.txt and changed Model.java

--- src/model/Model.java
+++ src/model/Model.java
@@ -2,14 +2,36 @@ package model;
 
 import controller.EventListener;
 
+import java.nio.file.Paths;
+
 /*
  * Этот класс будет отвечать за модель нашей игры
  */
 public class Model {
     public static final int FIELD_CELL_SIZE = 20;       // размер ячейки игрового поля
     EventListener eventListener;
+    private GameObjects gameObjects;                    // будет хранить игровые объекты
+    private int currentLevel = 1;                       // текущий уровень
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("res/levels.txt"));                    // загрузчик уровней
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restartLevel(currentLevel);
     }
 }

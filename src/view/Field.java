2c9334d9982aab34ad7cebd161243701331c9b18
DJ-UKOL <dj-ukol@yandex.ru>
2022-10-01 10:30:09 +0500

    Changed Field.java

--- src/view/Field.java
+++ src/view/Field.java
@@ -1,10 +1,13 @@
 package view;
 
 import controller.EventListener;
+import model.Direction;
 import model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public class Field extends JPanel {
@@ -13,6 +16,9 @@ public class Field extends JPanel {
 
     public Field(View view){
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        this.addKeyListener(keyHandler);                // установка слушателя клавиатуры
+        this.setFocusable(true);                        // установка фокуса
     }
 
     public void paint(Graphics g) {
@@ -29,4 +35,28 @@ public class Field extends JPanel {
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case KeyEvent.VK_UP:
+                    eventListener.move(Direction.UP);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case KeyEvent.VK_R:
+                    eventListener.restart();
+                    break;
+            }
+        }
+    }
 }
+
